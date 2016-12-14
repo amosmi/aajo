ee2aea7d2a0471f61caa643b4961c3d18c7214c9
amosmi <amos.miikkulainen@metropolia.fi>
2016-12-14 10:31:44 +0200

    Add files via upload

---
+++ valoS.java
@@ -0,0 +1,27 @@
+package valoS;
+
+
+
+import lejos.utility.Delay;
+import lejos.hardware.Button;
+
+public class valoS {
+boolean lopetaValotus=false;
+	public void valoShow() {
+		
+		while (lopetaValotus=false) {
+		Button.LEDPattern(7);
+		Delay.msDelay(200);
+		Button.LEDPattern(8);
+		Delay.msDelay(200);
+		Button.LEDPattern(9);
+		Delay.msDelay(200);
+		}
+
+	}
+	public void lopeta() {
+		lopetaValotus = true;
+}
+
+
+}
