8dc75c0f2bfad7897946746a2332d210febf904d
juhavuo <juha.vuokko@pp.inet.fi>
2016-12-14 19:24:19 +0200

    Add files via upload

--- BrickLukija.java
+++ BrickLukija.java
@@ -1,14 +1,24 @@
-package musa;
+package aajobotti;
 
 import lejos.hardware.Button;
 import lejos.utility.Delay;
 
+/**
+ * BrickLukija hoitaa brickin näppäinten painnallusten seuraamisen. BrickLukija on Thread-luokan aliluokka ja se hyödyntää
+ * ylikirjoittaa Thread-luokan run-metodin.
+ * @author JuhaVuokko
+ *
+ */
 public class BrickLukija extends Thread{
 	
 	private boolean lopeta;
 	private boolean rekisteroity;
 	private int nappainKoodi;
 	
+	/**
+	 * Luodaan Bricklukija-olio. Aluksi nappainkoodi on asetettu arvoon 0. Boolean-muuttujat lopeta ja rekisteroitu saa
+	 * arvot false.
+	 */
 	public BrickLukija(){
 		
 		this.lopeta = false;
@@ -17,16 +27,27 @@ public class BrickLukija extends Thread{
 		
 	}
 	
+	/**
+	 * Ylikirjoittaa Thread-luokan run()-metodin. Tämä hoitaa Brickin näppäinten seurannan.
+	 */
 	public void run(){
 		while(!this.lopeta){
 			this.nappainKoodi = Button.readButtons();
 		}
 	}
 	
+	/**
+	 * Lopeta-metodia tarvitaan run-metodin
+	 */
 	public void lopeta(){
 		this.lopeta = true;
 	}
 	
+	/**
+	 * Tämä palauttaa painalluksen mukaisen int-arvoisen koodin, painalluksen yhteydessä palauttaa vain yhden kerran painalluksen
+	 * koodia, muuten palautaa nollia.
+	 * @return int Näppäinpainallusten koodit.
+	 */
 	public int annaKoodi(){
 		if (this.rekisteroity == false && this.nappainKoodi != 0){
 			this.rekisteroity = true;
--- IRLukija.java
+++ IRLukija.java
@@ -1,35 +1,56 @@
-package musa;
+package aajobotti;
 
+import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3IRSensor;
+
+/**
+ * <h1> IRLukija </h1>
+ * IRLukija vastaa kaukos‰‰timen tilan lukemisesta. T‰ll‰ seurataan sit‰ painetaanko n‰pp‰imi‰ kaukos‰‰timest‰.
+ * @author JuhaVuokko
+ *
+ */
 
 public class IRLukija extends Thread {
 	private EV3IRSensor irsensori;
 	private int komento;
 	private boolean lopeta;
 	
+	/**
+	 * Luodaan IRLukija olio. 
+	 * @param sensori EV3IRSensor Tarvittava sensoriolio kaukos‰‰timen tilan seuraamiseen.
+	 */
 	public IRLukija(EV3IRSensor sensori){
 		this.irsensori = sensori;
 		this.lopeta = false;
 	}
-	// run-metodia suorittaessa tulee satunnaisia outofbounds-exceptioneita
-	// t‰llˆin komennoksi tulkitaan 0, jolloin mit‰‰n ei suoriteta
+	
+	/**
+	 * T‰m‰ metodi ylikirjoittaa Thread-luokan run()-metodin. T‰m‰ seuraa irsensorin avulla kaukos‰‰timen tilaa. 
+	 */
 	public void run(){
 		while (!this.lopeta){
 			try{
 				this.komento = this.irsensori.getRemoteCommand(0);
 			}catch(Exception e){
-				this.komento = 0;
+				LCD.drawString(e.toString(),0,0);
 			}
 			
 		}
 	}
 	
+	/**
+	 * T‰m‰ metodi v‰litt‰‰ int-muuttujana, mik‰ on kaukos‰‰timen tila: painetaanko jotain n‰pp‰int‰ tai ei.
+	 * @return int n‰pp‰inkoodi int-muodossa.
+	 */
 	public int annaKomento(){
 		return this.komento;
 	}
 	
+	/**
+	 * T‰t‰ metodia tarvitaan threadin pys‰ytt‰miseksi. Ilman t‰m‰n k‰yttˆ‰, ohjelmaa ei saada sammutettua.
+	 */
 	public void lopeta(){
 		this.lopeta = true;
 	}
-
+	
 }
